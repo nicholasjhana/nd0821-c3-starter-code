4c095fd3b951c25b9000e069c07f60b081628f7b
nicholasjhana <nickashaw3@gmail.com>
2021-12-05 21:51:52 +0100

    bug fix

--- starter/main.py
+++ starter/main.py
@@ -83,14 +83,14 @@ async def create_item(sample_id: int, sample: Sample):
 
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("aws configure --profile 'udacity'")
-    os.system("dvc remote add - d remotestores3 ${{env.DVC_REMOTE_REPOSITORY}}")
+    # os.system("aws configure --profile 'udacity'")
     os.system("dvc config core.no_scm true")
+    # os.system("dvc remote add -d remotestores3 ${{env.DVC_REMOTE_REPOSITORY}}")
     # if os.system("dvc pull") != 0:
     #     exit("dvc pull failed")
-    dvc_output = subprocess.run(["dvc", "pull", "--remote remotestores3"], capture_output=True, text=True)
+    dvc_output = subprocess.run(["dvc", "pull"], capture_output=True, text=True)
     print(dvc_output.stdout)
     print(dvc_output.stderr)
     if dvc_output.returncode != 0:
         print("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
+    # os.system("rm -r .dvc .apt/usr/lib/dvc")
