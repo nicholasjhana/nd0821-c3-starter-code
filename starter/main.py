1b0a500a091c7234db9d768c5582e9f97d9a665f
nicholasjhana <nickashaw3@gmail.com>
2021-12-05 20:52:42 +0100

    add aws profile to main

--- starter/main.py
+++ starter/main.py
@@ -83,6 +83,7 @@ async def create_item(sample_id: int, sample: Sample):
 
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("aws configure --profile 'udacity'")
     os.system("dvc config core.no_scm true")
     # if os.system("dvc pull") != 0:
     #     exit("dvc pull failed")
