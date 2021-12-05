be0c804ece3171300179622fd332226de8ab58d6
nicholasjhana <nickashaw3@gmail.com>
2021-12-05 21:38:26 +0100

    dvc specify remote

--- starter/main.py
+++ starter/main.py
@@ -84,6 +84,7 @@ async def create_item(sample_id: int, sample: Sample):
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("aws configure --profile 'udacity'")
+    os.system("dvc remote add - d remotestores3 ${{env.DVC_REMOTE_REPOSITORY}}")
     os.system("dvc config core.no_scm true")
     # if os.system("dvc pull") != 0:
     #     exit("dvc pull failed")
