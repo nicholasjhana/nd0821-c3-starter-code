d4c83ef8f690b7caf7c1a558c527a238dad39db9
nicholasjhana <nickashaw3@gmail.com>
2021-12-05 22:14:06 +0100

    remove profile aws

--- starter/main.py
+++ starter/main.py
@@ -83,9 +83,11 @@ async def create_item(sample_id: int, sample: Sample):
 
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("aws configure --profile 'udacity'")
+    # os.system("aws configure --profile 'udacity'")
     os.system("dvc remote add remotestores3 ${{env.DVC_REMOTE_REPOSITORY}}")
-    os.system("dvc config core.no_scm true core.remote remotestores3")
+    os.system("dvc config core.remote remotestores3")
+    os.system("dvc config core.no_scm true")
+
 
     # if os.system("dvc pull") != 0:
     #     exit("dvc pull failed")
